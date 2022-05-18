09739d67c51eb43348358af899b1136d57aa20ec
Kamaal <kamaal.f1@gmail.com>
2022-05-18 21:23:46 +0200

    Fix checkbox popup

--- js/script.js
+++ js/script.js
@@ -2,31 +2,18 @@ let navbar = document.querySelector('.navbar')
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
-    searchForm.classList.remove('active');
 }
 
-
-// let loginForm = document.querySelector('.login-form')
-
 // document.querySelector('#login-btn').onclick = () =>{
-//     loginForm.classList.toggle('active');
 //     navbar.classList.remove('active');
-//     searchForm.classList.remove('active');
 // }
 
-// let searchForm = document.querySelector('.search-form')
-
 // document.querySelector('#search-btn').onclick = () =>{
-//     searchForm.classList.toggle('active');
 //     navbar.classList.remove('active');
-//     loginForm.classList.remove('active');
 // }
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-    searchForm.classList.remove('active');
 }
 
 let themeBtn = document.querySelector('#theme-btn')
