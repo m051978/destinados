e9cfd97797a03e22bb2a683a8890059839295e94
m051978 <103782540+m051978@users.noreply.github.com>
2022-05-19 22:15:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,76 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    // loginForm.classList.remove('active');
+    // searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    // loginForm.classList.remove('active');
+    // searchForm.classList.remove('active');
+}
+
+let themeBtn = document.querySelector('#theme-btn')
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun')
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+};
+
+
+var swiper = new Swiper(".review-slider",{
+    loop:true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay:5500,
+        dusableOnInteraction: false,
+    }, 
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
+
+
+//   AT LEAST ONE CHECKBOX MUST BE TICKED
+
+(function() {
+    const form = document.querySelector('.check-boxes');
+    const checkboxes = form.querySelectorAll('input[type=checkbox]');
+    const checkboxLength = checkboxes.length;
+    const firstCheckbox = checkboxLength > 0 ? checkboxes[0] : null;
+
+    function init() {
+        if (firstCheckbox) {
+            for (let i = 0; i < checkboxLength; i++) {
+                checkboxes[i].addEventListener('change', checkValidity);
+            }
+
+            checkValidity();
+        }
+    }
+
+    function isChecked() {
+        for (let i = 0; i < checkboxLength; i++) {
+            if (checkboxes[i].checked) return true;
+        }
+
+        return false;
+    }
+
+    function checkValidity() {
+        const errorMessage = !isChecked() ? 'Al menos una casilla de verificación debe estar seleccionada.' : '';
+        firstCheckbox.setCustomValidity(errorMessage);
+    }
+
+    init();
+})();
